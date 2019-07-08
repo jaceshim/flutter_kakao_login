aae13104702e13a1486128fc7dc2fe4cc4eff6e6
Jace Shim <jaceshim@Jaceui-MacBook-Pro.local>
2019-07-08 23:05:30 +0900

    android쪽 disabled

--- android/src/main/java/com/josephnk/flutterkakaologin/FlutterKakaoLoginPlugin.java
+++ android/src/main/java/com/josephnk/flutterkakaologin/FlutterKakaoLoginPlugin.java
@@ -1,50 +1,25 @@
 package com.josephnk.flutterkakaologin;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.util.Log;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/**
- * FlutterKakaoLoginPlugin
- */
-public class FlutterKakaoLoginPlugin implements MethodCallHandler, PluginRegistry.ActivityResultListener {
-
-  private static final String CHANNEL_NAME = "flutter_kakao_login";
-
-  private static final String METHOD_LOG_IN = "logIn";
-  private static final String METHOD_LOG_OUT = "logOut";
-  private static final String METHOD_GET_CURRENT_ACCESS_TOKEN = "getCurrentAccessToken";
-  private static final String METHOD_GET_USER_ME = "getUserMe";
-
-  private static final String LOG_TAG = "KakaoTalkPlugin";
-
-  private Activity currentActivity;
-  private SessionCallback sessionCallback;
-
-  /**
-   * Plugin registration.
-   */
+/** FlutterPlugin */
+public class FlutterKakaoLoginPlugin implements MethodCallHandler {
+  /** Plugin registration. */
   public static void registerWith(Registrar registrar) {
-    final FlutterKakaoLoginPlugin plugin = new FlutterKakaoLoginPlugin(registrar);
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), CHANNEL_NAME);
-    channel.setMethodCallHandler(plugin);
-    registrar.addActivityResultListener(plugin);
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_kakao_login");
+    channel.setMethodCallHandler(new FlutterPlugin());
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
-    final Result _result = result;
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
   }
 }
